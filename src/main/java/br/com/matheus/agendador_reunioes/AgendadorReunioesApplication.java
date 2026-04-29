b4307f7d06e3cf13c90d581d76286491cc324a42
Matheus Schalch <matheus.schalch@gmail.com>
2026-04-29 14:05:31 -0300

    chore: initial commit com estrutura hexagonal

---
+++ src/main/java/br/com/matheus/agendador_reunioes/AgendadorReunioesApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheus.agendador_reunioes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorReunioesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorReunioesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheus/agendador_reunioes/AgendadorReunioesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.agendador_reunioes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorReunioesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
